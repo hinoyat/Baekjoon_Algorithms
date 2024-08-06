180036c5eb87fdd0c4cd452e0e63424cd07eff03
Hinoyat <hhwj2270@gmail.com>
2024-08-06 22:47:25 +0900

    270806 2247

--- step-by-step/99.for im.py
+++ step-by-step/99.for im.py
@@ -59,18 +59,174 @@ from pprint import pprint
 #     print(c)
 
 
-# 13300 방 배정
+# # 13300 방 배정
 
-N, K = map(int, input().split())
-lst = []
-dct = {}
-for _ in range(N):
-    S_Y = tuple(map(int, input().split()))
-    lst.append(S_Y)
+# N, K = map(int, input().split())
+# lst = []
+# dct = {}
+# for _ in range(N):
+#     S_Y = tuple(map(int, input().split()))
+#     lst.append(S_Y)
 
-for i in lst:
-    if i not in dct :
-        dct[i] = 1
-    else:
-        dct[i] += 1
+# for i in lst:
+#     if i not in dct :
+#         dct[i] = 1
+#     else:
+#         dct[i] += 1
 
+# room = 0
+# for d in dct.keys():
+#     room += dct[d]//K
+#     if dct[d] %  K >0:
+#         room += 1
+
+# print(room)
+
+
+# # 2605 줄 세우기
+# N = int(input())
+# stdnt = [i for i in range(1, N+1)]
+
+# num = list(map(int, input().split()))
+
+# for i in range(N):
+#     if num[i] == 0:
+#         pass
+#     else:
+#         for j in range(num[i]):
+#             stdnt[i-num[i]+j], stdnt[i] = stdnt[i], stdnt[i-num[i]+j]
+# print(*stdnt)
+
+# # 14696 딱지놀이
+# N = int(input())
+# for round in range(1, N+1):
+#     A = list(map(int, input().split()))
+#     B = list(map(int, input().split()))
+#     a_num = A.pop(0)
+#     b_num = B.pop(0)
+#     # print(A, a_num)
+#     # print(B, b_num)
+
+#     winner = 'D'
+
+#     for i in range(4, 0, -1):
+#         if A.count(i) > B.count(i):
+#             winner = 'A'
+#             break
+#         elif A.count(i) < B.count(i):
+#             winner = 'B'
+#             break
+#         else:
+#             pass
+#     print(winner)
+
+# # 2628 종이 자르기
+
+# row, col = map(int, input().split())
+# N = int(input())
+
+# r_lst = []
+# c_lst = []
+
+# for cut in range(N):
+#     r_c, num = map(int, input().split())
+#     if r_c == 0:
+#         c_lst.append(num)
+#     else:
+#         r_lst.append(num)
+
+# row_lst = [0] + r_lst + [row]
+# col_lst = [0] + c_lst + [col]
+
+
+# row_lst.sort()
+# col_lst.sort()
+
+# max_row = 0
+# max_col = 0
+# # 행 길이 최댓값
+# for i in range(len(row_lst)-1):
+#     if row_lst[i+1] - row_lst[i] > max_row:
+#         max_row = row_lst[i+1] - row_lst[i]
+# # 열 길이 최댓값
+# for j in range(len(col_lst)-1):
+#     if col_lst[j+1] - col_lst[j] > max_col:
+#         max_col = col_lst[j+1] - col_lst[j]
+
+
+# result = max_row * max_col
+
+# print(result)
+
+
+# # 2578 비잉고오
+# arr_a = [list(map(int, input().split())) for _ in range(5)]
+
+# lst_b = []
+# for b in range(5):
+#     b = list(map(int, input().split()))
+#     for b_1 in b:
+#         lst_b.append(b_1)
+
+
+# cnt = 0
+# b_idx = 0
+# ar1 = 0
+# ar2 = 0
+# ar3 = 0
+# ar4 = 0
+
+
+# binggo = False
+# while binggo == False:
+#         cnt += 1
+#         num = lst_b[b_idx]
+#         for i in range(5):
+#             for j in range(5):
+#                 if arr_a[i][j] == num:
+#                     arr_a[i][j] = 0
+
+#         b_idx += 1
+#         # 행 합
+#         for i in range(5):
+#             if sum(arr_a[i]) == 0:
+#                 ar1 += 1
+
+#         # 열 합
+#         for i in range(5):
+#             sum2 = 0
+#             for j in range(5):
+#                 sum2 += arr_a[j][i]
+#             if sum2 == 0:
+#                 ar2 +=1
+#         # 대각
+#         sum3 = 0
+#         for i in range(5):
+#             for j in range(5):
+#                 if i==j:
+#                     sum3 += arr_a[i][j]
+#         if sum3 == 0:
+#             ar3 +=1
+
+#         # 역대각
+#         sum4 = 0
+#         for i in range(5):
+#             for j in range(5):
+#                 if 5-i-1==j:
+#                     sum4 += arr_a[i][j]
+#         if sum4 == 0:
+#             ar4 +=1
+
+#         # 종료        
+#         if ar1 + ar2 + ar3 + ar4 >= 3:
+#             binggo = True
+#         else:
+#             ar1 = ar2 = ar3 = ar4 = 0
+
+# # print(binggo, cnt)
+# # print(ar1, ar2, ar3, ar4)
+# # pprint(arr_a)
+# print(cnt)
+
+    
+# # pprint(arr_a)
--- step-by-step/study.py
+++ step-by-step/study.py
@@ -1,37 +1,75 @@
 from pprint import pprint
 
-#2669 직사각형 네개의 합집합의 면적 구하기
+# #2669 직사각형 네개의 합집합의 면적 구하기
 
-# 면적의 최대
+# # 면적의 최대
 
-arr_len = 100
-arr = [[0 for _ in range(arr_len)]for _ in range(arr_len)]
+# arr_len = 100
+# arr = [[0 for _ in range(arr_len)]for _ in range(arr_len)]
 
-# 정수를 몇 번 입력하는지
-test = 4
+# # 정수를 몇 번 입력하는지
+# test = 4
 
-# test 동안 사각형 입력
-for tc in range(test):
-    co_lst = list(map(int,input().split()))
-    # [x1, y1, x2, y2]
-    # [1 , 2 , 3 , 4 ]
+# # test 동안 사각형 입력
+# for tc in range(test):
+#     co_lst = list(map(int,input().split()))
+#     # [x1, y1, x2, y2]
+#     # [1 , 2 , 3 , 4 ]
     
-    for i in range(co_lst[0], co_lst[2]):
-        for j in range(co_lst[1], co_lst[3]):
-            arr[j][i] = 1
-    # print(co_lst)
-    # co_lst.clear()  필요할 줄 알았는데 for문 돌 때마다 갱신되는 것을 알았습니다.
+#     for i in range(co_lst[0], co_lst[2]):
+#         for j in range(co_lst[1], co_lst[3]):
+#             arr[j][i] = 1
+#     # print(co_lst)
+#     # co_lst.clear()  필요할 줄 알았는데 for문 돌 때마다 갱신되는 것을 알았습니다.
 
-# pprint(arr)
+# # pprint(arr)
 
-# 면적의 합
-sum = 0
+# # 면적의 합
+# sum = 0
 
-# 2차원 배의 행을 하나씩 뜯어서
-for s in arr:
-    #하나씩 1인지 더했어요!
-    for i in range(arr_len):
-        if s[i] == 1:
-            sum += 1
+# # 2차원 배의 행을 하나씩 뜯어서
+# for s in arr:
+#     #하나씩 1인지 더했어요!
+#     for i in range(arr_len):
+#         if s[i] == 1:
+#             sum += 1
 
-print(sum)
+# print(sum)
+
+
+# # 종이 자르기
+# row, col = map(int, input().split())
+# N = int(input())
+
+# r_lst = []
+# c_lst = []
+
+# for cut in range(N):
+#     r_c, num = map(int, input().split())
+#     if r_c == 0:
+#         c_lst.append(num)
+#     else:
+#         r_lst.append(num)
+
+# row_lst = [0] + r_lst + [row]
+# col_lst = [0] + c_lst + [col]
+
+
+# row_lst.sort()
+# col_lst.sort()
+
+# max_row = 0
+# max_col = 0
+# # 행 길이 최댓값
+# for i in range(len(row_lst)-1):
+#     if row_lst[i+1] - row_lst[i] > max_row:
+#         max_row = row_lst[i+1] - row_lst[i]
+# # 열 길이 최댓값
+# for j in range(len(col_lst)-1):
+#     if col_lst[j+1] - col_lst[j] > max_col:
+#         max_col = col_lst[j+1] - col_lst[j]
+
+
+# result = max_row * max_col
+
+# print(result)
