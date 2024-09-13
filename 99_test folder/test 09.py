ee1a90ebcc10856dad24fb3597a898f186bfa314
Hinoyat <hhwj2270@gmail.com>
2024-09-13 23:23:37 +0900

    240913 1431

--- 99_test folder/test 09.py
+++ 99_test folder/test 09.py
@@ -18,16 +18,36 @@
 # print(ans)
 #
 
+#
+# import sys
+# input = sys.stdin.readline
+# N, M = map(int, input().split())
+# num = list(map(int, input().split()))
+# new_lst = [0] * (N + 1)
+#
+# for i in range(1, N + 1):
+#     new_lst[i] = new_lst[i - 1] + num[i - 1]
+# # print(new_lst)
+# for _ in range(M):
+#     a, b = map(int, input().split())
+#     print(new_lst[b] - new_lst[a - 1])
+
 
 import sys
 input = sys.stdin.readline
 N, M = map(int, input().split())
-num = list(map(int, input().split()))
-new_lst = [0] * (N + 1)
+lst = [int(input()) for _ in range(N)]
+# print(lst)
+idx = N - 1
+ans = 0
+while M > 0:
+    if lst[idx] <= M:
+        ans += M // lst[idx]
+        # print(M, lst[idx])
+        M %= lst[idx]
 
-for i in range(1, N + 1):
-    new_lst[i] = new_lst[i - 1] + num[i - 1]
-# print(new_lst)
-for _ in range(M):
-    a, b = map(int, input().split())
-    print(new_lst[b] - new_lst[a - 1])
+        # print(M)
+    idx -= 1
+    if idx == -1:
+        break
+print(ans)
