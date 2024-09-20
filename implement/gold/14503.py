fbe1ff4acb43ccb666d51455710343d86b623f1f
Hinoyat <hhwj2270@gmail.com>
2024-09-20 15:36:26 +0900

    240920 1520

--- implement/gold/14503.py
+++ implement/gold/14503.py
@@ -1,14 +1,41 @@
 def robo(si, sj, di):
     visited = [[0] * M for _ in range(N)]
     visited[si][sj] = 1
-    i = si
-    j = sj
-    d = di
+    ans = 1
+    ni = si
+    nj = sj
+    nd = di
+    while True:
+        # di, dj = direction[nd][0], direction[nd][1]
 
-    pass
+        # 현재 칸에서 주변을 돌아보기
+        can = False
+        cnt = 0
+        while cnt <= 3:
+            nd = direction[nd][2]
+            if nd >= 4:
+                nd = 0
+            ci, cj = ni + direction[nd][0], nj + direction[nd][1]
+            if 0 <= ci < N and 0 <= cj < M:
+                if arr[ci][cj] == 0 and visited[ci][cj] == 0:
+                    can = True
+                    ni, nj = ci, cj
+                    break
+                else:
+                    cnt += 1
+            else:
+                cnt += 1
+
+        if can == False:
+            ni, nj = ni - direction[nd][0], nj - direction[nd][1]
+            if arr[ni][nj] == 1:
+                return ans
+        else:
+            visited[ni][nj] = 1
+            ans += 1
 
 
-N, M = int(input())
+N, M = map(int, input().split())
 robox, roboy, firstd = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 dir = {0:(1, 0, 3), 1:(0, 1, 0), 2:(-1, 0, 1), 3:(0, -1, 2)}
@@ -22,3 +49,10 @@ dir = {0:(1, 0, 3), 1:(0, 1, 0), 2:(-1, 0, 1), 3:(0, -1, 2)}
 # 빈칸이 있으면
 # 1. 90도 회전
 # 2. 한칸 전진
+
+
+# 현재 칸이 청소 X 청소 -> 방문처리 청소는 -1로 하자
+
+direction = {0 : (-1, 0, 3), 1 : (0, 1, 0), 2 : (1, 0, 1), 3 : (0, -1, 2)}
+
+print(robo(robox, roboy, firstd))
